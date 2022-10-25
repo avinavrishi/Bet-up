e4b08efcffd34db3c13aebd47f4ce5fca2a401bc
avinavrishi <30436946+avinavrishi@users.noreply.github.com>
2022-10-25 16:27:44 +0530

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from django.conf.urls import url 
-from .views import Home, Result_list
+from .views import Home, gameView, finalResults
 from accounts import views
 
 urlpatterns = [
     path("", Home.as_view(), name="home"),
-    url(r'^api/result$', views.Result_list),
+    url("gameView", views.gameView, name = "gameView"),
+    url("final", views.finalResults, name = "result")
 ]
