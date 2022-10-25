bdfe2e0de14275184a020c514c95fe6719985f47
avinavrishi <30436946+avinavrishi@users.noreply.github.com>
2022-10-25 16:24:57 +0530

    Update views.py

--- accounts/views.py
+++ accounts/views.py
@@ -2,22 +2,26 @@ from django.views.generic import TemplateView
 from django.views.generic.edit import CreateView
 from django.urls import reverse_lazy
 from .forms import RegistrationForm
-
 from django.shortcuts import render
+from django.http import HttpResponse
 
-from django.http.response import JsonResponse
-from rest_framework.parsers import JSONParser 
-from rest_framework import status
+# from django.shortcuts import render
+
+# from django.http.response import JsonResponse
+# from rest_framework.parsers import JSONParser 
+# from rest_framework import status
  
-from accounts.models import Result
-from accounts.serializers import ResultSerializer
-from rest_framework.decorators import api_view
+# # from accounts.models import Result
+# from accounts.serializers import ResultSerializer
+# from rest_framework.decorators import api_view
 import json
-from .models import event, transaction
+from .models import event, transaction, finalResult
 from datetime import datetime
 
 class Home(TemplateView):
     template_name = "home.html"
+    
+    extra_context={'events': finalResult.objects.all()}
 
 
 class SignUpView(CreateView):
@@ -42,13 +46,19 @@ class SignUpView(CreateView):
 #         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 def gameView(request):
-    if request.user.is_Authenticated:
+    if request.user.is_authenticated:
+
         #handle authentication
         if request.method=='POST':
+
             #In post request send amount, event_id, colour
-            postData = json.loads(request.body)
-            amount = postData.amount
-            colour = postData.colour
+            #postData = json.loads(request.body)
+
+            print(request.body)
+            
+            amount = request.POST['amount']
+
+            colour = request.POST['colour']
             event_obj = postData.event_id
             event_obj = event.object.get(id=event)
             transaction_obj = transaction(user=request.user, amount=amount, colour=colour, event=event_obj)
@@ -80,3 +90,22 @@ def gameView(request):
         #handle authentication
         pass
             
+
+def finalResults(request):
+    winner = event.objects.latest('id')
+    redCount = winner.red_count
+    blueCount = winner.blue_count
+    greenCount = winner.green_count
+    if(redCount < blueCount and redCount < greenCount):
+        print("red")
+        finalResult_obj = finalResult(event = winner.id, colour= "red")
+
+    elif(blueCount < greenCount and blueCount < redCount):
+        print("blue")
+        finalResult_obj = finalResult(event = winner.id, colour= "blue")
+    else:
+        print("Green")
+        finalResult_obj = finalResult(event = winner.id, colour= "green")
+    finalResult_obj.save()
+
+    return render(request, "home.html") 
